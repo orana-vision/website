865b0267c97032933d8aaaf0489ef9cbea5e742a
giannisgkountras <gkountrasioannis@gmail.com>
2022-09-01 20:00:06 +0300

    carousel

--- src/components/Header/VerticalCarousel.js
+++ src/components/Header/VerticalCarousel.js
@@ -6,64 +6,20 @@ export default function VerticalCarousel() {
       <div class="carousel">
         <div class="carousel__item">
           <div class="carousel__item-body">
-            <p class="title">spouting whale</p>
+            <p class="title">1</p>
             <p>Unicode: U+1F433</p>
           </div>
         </div>
         <div class="carousel__item">
-          <div class="carousel__item-head">🐋</div>
           <div class="carousel__item-body">
-            <p class="title">whale</p>
+            <p class="title">2</p>
             <p>Unicode: U+1F40B</p>
           </div>
         </div>
         <div class="carousel__item">
-          <div class="carousel__item-head">🐬</div>
           <div class="carousel__item-body">
-            <p class="title">dolphin</p>
+            <p class="title">3</p>
             <p>Unicode: U+1F42C</p>
-          </div>
-        </div>
-        <div class="carousel__item">
-          <div class="carousel__item-head">🐟</div>
-          <div class="carousel__item-body">
-            <p class="title">fish</p>
-            <p>Unicode: U+1F41F</p>
-          </div>
-        </div>
-        <div class="carousel__item">
-          <div class="carousel__item-head">🐠</div>
-          <div class="carousel__item-body">
-            <p class="title">tropical fish</p>
-            <p>Unicode: U+1F420</p>
-          </div>
-        </div>
-        <div class="carousel__item">
-          <div class="carousel__item-head">🐡</div>
-          <div class="carousel__item-body">
-            <p class="title">blowfish</p>
-            <p>Unicode: U+1F421</p>
-          </div>
-        </div>
-        <div class="carousel__item">
-          <div class="carousel__item-head">🦈</div>
-          <div class="carousel__item-body">
-            <p class="title">shark</p>
-            <p>Unicode: U+1F988</p>
-          </div>
-        </div>
-        <div class="carousel__item">
-          <div class="carousel__item-head">🐙</div>
-          <div class="carousel__item-body">
-            <p class="title">octopus</p>
-            <p>Unicode: U+1F419</p>
-          </div>
-        </div>
-        <div class="carousel__item">
-          <div class="carousel__item-head">🐚</div>
-          <div class="carousel__item-body">
-            <p class="title">spiral shell</p>
-            <p>Unicode: U+1F41A</p>
           </div>
         </div>
       </div>
