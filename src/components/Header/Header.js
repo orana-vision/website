f591a965081e66244923e024b1414d3b83949398
giannisgkountras <gkountrasioannis@gmail.com>
2022-09-01 19:48:39 +0300

    carousel

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,39 +3,49 @@ import "./Header.css";
 import LOGO from "../../assets/orana-logo.png";
 import { data } from "../../data";
 import PROJ from "../../assets/header-demo.png";
+import VerticalCarousel from "./VerticalCarousel";
 
 const Header = () => {
-    const [imageHover, setImageHover] = useState(false);
+  const [imageHover, setImageHover] = useState(false);
 
-    const handleMouseOut = () => {
-        setImageHover(false);
-    };
+  const handleMouseOut = () => {
+    setImageHover(false);
+  };
 
-    const handleMouseOver = () => {
-        setImageHover(true);
-    };
+  const handleMouseOver = () => {
+    setImageHover(true);
+  };
 
-    return (
-        <section id="header">
-            <img src={LOGO} alt="logo" className="logo" />
-            <h1 className="typewriter">the New Vision.</h1>
-            <div>
-                <button className="border-gradient border-gradient-card">
-                    <a href="#experience">See Projects</a>
-                </button>
-                <button className="filled">
-                    <a href="#contact">Let's talk</a>
-                </button>
-            </div>
-            <a href="#experience">
-                <div className="project-demo" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-                    {/* <div id="desc">{data.headerDemo.name}</div> */}
-                    <img src={PROJ} alt={data.headerDemo.name} className={imageHover ? "open" : "close"}></img>
-                    {data.headerDemo.name}
-                </div>
-            </a>
-        </section>
-    );
+  return (
+    <section id="header">
+      <div className="row">
+        <div>
+          <img src={LOGO} alt="logo" className="logo" />
+          <h1 className="typewriter">the New Vision.</h1>
+        </div>
+
+        <div className="info">
+          {" "}
+          <VerticalCarousel></VerticalCarousel>
+        </div>
+      </div>
+      <div>
+        <button className="border-gradient border-gradient-card">
+          <a href="#experience">See Projects</a>
+        </button>
+        <button className="filled">
+          <a href="#contact">Let's talk</a>
+        </button>
+      </div>
+      <a href="#experience">
+        <div className="project-demo" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+          {/* <div id="desc">{data.headerDemo.name}</div> */}
+          <img src={PROJ} alt={data.headerDemo.name} className={imageHover ? "open" : "close"}></img>
+          {data.headerDemo.name}
+        </div>
+      </a>
+    </section>
+  );
 };
 
 export default Header;
