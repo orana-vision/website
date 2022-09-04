c51e61bf4d3e1a4d222b3ac75faba3ee24463b01
Ioannis Horgos <jhor@acta.com.gr>
2022-09-04 21:26:15 +0300

    about cards

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { BsGlobe2, BsPhone } from "react-icons/bs";
 import { AiOutlineShopping } from "react-icons/ai";
 import "./About.css";
+import websiteSvg from "../../assets/undraw_add_color_re_buro.svg";
+import mobileSvg from "../../assets/undraw_mobile_analytics_72sr.svg";
+import eshopSvg from "../../assets/undraw_online_test_re_kyfx.svg";
+import HORGOS from "../../assets/horgos.jpg";
 
 function a11yProps(index) {
     return {
@@ -47,38 +51,54 @@ const About = () => {
     };
 
     return (
-        <section className="container" id="about">
-            About
+        <section id="about">
+            <div className="about-header">About</div>
             <div className="services-container">
                 <div
                     className="service"
                     onMouseEnter={() => handleInHover("phone")}
                     onMouseLeave={() => handleOutHover("phone")}>
-                    <div className={hover.phoneHover ? "icons flip-in" : "icons"}>
+                    {/* <div className={hover.phoneHover ? "icons flip-in" : "icons"}>
                         <BsPhone style={{ color: "#06A77D" }} className="img-phone" />
-                    </div>
+                    </div> */}
+                    <img src={mobileSvg} className="svg-icon mobile-svg" />
+
                     <div className="text">We develop custom mobile apps that can help your bussiness.</div>
                 </div>
                 <div
                     className="service"
                     onMouseEnter={() => handleInHover("web")}
                     onMouseLeave={() => handleOutHover("web")}>
-                    <div className={hover.webHover ? "icons flip-in" : "icons"}>
+                    {/* <div className={hover.webHover ? "icons flip-in" : "icons"}>
                         <BsGlobe2 style={{ color: "#9395D3" }} />
-                    </div>
+                    </div> */}
+                    <img src={websiteSvg} className="svg-icon" />
+
                     <div className="text">We develop custom mobile apps that can help your bussiness.</div>
                 </div>
                 <div
                     className="service"
                     onMouseEnter={() => handleInHover("eshop")}
                     onMouseLeave={() => handleOutHover("eshop")}>
-                    <div className={hover.eshopHover ? "icons flip-in" : "icons"}>
+                    {/* <div className={hover.eshopHover ? "icons flip-in" : "icons"}>
                         <AiOutlineShopping style={{ color: "#F1A208" }} />
-                    </div>
+                    </div> */}
+                    <img src={eshopSvg} className="svg-icon" />
                     <div className="text">We develop custom mobile apps that can help your bussiness.</div>
                 </div>
             </div>
-            <div></div>
+            <div className="meet-team-container">
+                <div className="team-member">
+                    <img src={HORGOS} alt="horgos" />
+                    <span>Giannis Horgos</span>
+                </div>
+                <div className="team-member">
+                    <img src={HORGOS} alt="horgos" />
+                </div>
+                <div className="team-member">
+                    <img src={HORGOS} alt="horgos" />
+                </div>
+            </div>
         </section>
     );
 };
