9659b75334198f2cf0cee86470f283d220b80695
Ioannis Horgos <jhor@acta.com.gr>
2022-09-04 21:40:00 +0300

    fixed bugs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,52 +5,51 @@ import { data } from "../../data";
 import PROJ from "../../assets/header-demo.png";
 import VerticalCarousel from "./VerticalCarousel";
 import Waves from "./Waves";
-import { useNavigate, Link } from "react-router-dom";
 
 const Header = () => {
-  const [imageHover, setImageHover] = useState(false);
+    const [imageHover, setImageHover] = useState(false);
 
-  const handleMouseOut = () => {
-    setImageHover(false);
-  };
+    const handleMouseOut = () => {
+        setImageHover(false);
+    };
 
-  const handleMouseOver = () => {
-    setImageHover(true);
-  };
-  return (
-    <section id="header">
-      <div className="row">
-        <div className="logo-container">
-          <img src={LOGO} alt="logo" className="logo" />
-          <h1 className="typewriter">the New Vision.</h1>
-        </div>
+    const handleMouseOver = () => {
+        setImageHover(true);
+    };
+    return (
+        <section id="header">
+            <div className="row">
+                <div className="logo-container">
+                    <img src={LOGO} alt="logo" className="logo" />
+                    <h1 className="typewriter">the New Vision.</h1>
+                </div>
 
-        <div className="info">
-          <VerticalCarousel />
-        </div>
-      </div>
-      <div className="bottom-container">
-        <div className="buttons-container">
-          <button className="neomorphism filled">
-            <a href="#experience">See Projects</a>
-          </button>
-          <button className="neomorphism">
-            <a href="#contact">Let's talk</a>
-          </button>
-        </div>
-        <div className="waves">
-          <Waves />
-        </div>
-      </div>
-      {/* <a href="#experience">
+                <div className="info">
+                    <VerticalCarousel />
+                </div>
+            </div>
+            <div className="bottom-container">
+                <div className="buttons-container">
+                    <button className="neomorphism filled">
+                        <a href="#experience">See Projects</a>
+                    </button>
+                    <button className="neomorphism">
+                        <a href="#contact">Let's talk</a>
+                    </button>
+                </div>
+                <div className="waves">
+                    <Waves />
+                </div>
+            </div>
+            {/* <a href="#experience">
                 <div className="project-demo" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
                     <div id="desc">{data.headerDemo.name}</div>
                     <img src={PROJ} alt={data.headerDemo.name} className={imageHover ? "open" : "close"}></img>
                     {data.headerDemo.name}
                 </div>
             </a> */}
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Header;
