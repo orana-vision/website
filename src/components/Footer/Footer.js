110dc811da9311b73fda30306698e9ac4b522d7b
Ioannis Horgos <jhor@acta.com.gr>
2022-09-15 23:04:24 +0300

    contact form updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { RiContactsFill } from "react-icons/ri";
 import "./Footer.css";
 const Footer = () => {
-    return <section id="footer">footer</section>;
+    return (
+        <section id="footer">
+            <div>
+                <div className="contact-icon">
+                    Contact Us!
+                    <RiContactsFill />
+                </div>
+                <div className="contact-info">
+                    <span>Mobile</span>
+                    <span>Email</span>
+                    <span>Social Media</span>
+                </div>
+            </div>
+            <div>2</div>
+            <div>3</div>
+        </section>
+    );
 };
 
 export default Footer;
