2e6795c27fe3179c04af719d244aded9cc9ba971
Ioannis Horgos <jhor@acta.com.gr>
2022-09-16 01:18:02 +0300

    footer-section updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,55 @@
 import React from "react";
 import { RiContactsFill } from "react-icons/ri";
+import { BsFillTelephoneFill, BsLinkedin } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+
 import "./Footer.css";
 const Footer = () => {
     return (
         <section id="footer">
-            <div>
-                <div className="contact-icon">
-                    Contact Us!
-                    <RiContactsFill />
+            <div className="info">
+                <div className="footer-childs">
+                    <div className="contact-icon">
+                        <p className="section-title">
+                            Contact Us!
+                            <RiContactsFill />
+                        </p>
+                    </div>
+                    <div className="contact-info">
+                        <span>
+                            <BsFillTelephoneFill className="footer-icons" />
+                            <p>6946930146</p>
+                        </span>
+                        <span>
+                            <MdEmail className="footer-icons" /> <p>orana.vision@gmail.com</p>
+                        </span>
+                        <span>
+                            <BsLinkedin className="footer-icons" /> <p>LinkedIn</p>
+                        </span>
+                    </div>
                 </div>
-                <div className="contact-info">
-                    <span>Mobile</span>
-                    <span>Email</span>
-                    <span>Social Media</span>
+                <div className="footer-childs">
+                    <p className="section-title">Navigate</p>
+                    <div className="contact-info">
+                        <span>
+                            <a href="#header">Home</a>
+                        </span>
+                        <span>
+                            <a href="#about">About</a>
+                        </span>
+                        <span>
+                            <a href="#experience">Experience</a>
+                        </span>
+                        <span>
+                            <a href="#contact">Contact</a>
+                        </span>
+                    </div>
                 </div>
             </div>
-            <div>2</div>
-            <div>3</div>
+            <div className="copyrights">
+                <div className="line-separator"></div>
+                <div className="details">© 2022 Orana - the New Vision, All rights reserved.</div>
+            </div>
         </section>
     );
 };
