91c292a68203a219aace6d81d84b9855e8eec80a
giannisgkountras <gkountrasioannis@gmail.com>
2022-09-16 05:04:57 +0300

    disabled scrolled, added back to orana

--- src/components/Team/Gkountras.js
+++ src/components/Team/Gkountras.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ME from "../../assets/me.jpg";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import "./Horgos.css";
+import git from "../../assets/github.png";
 
 const Gkountras = ({ setSelected }) => {
+  var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+  function preventDefault(e) {
+    e.preventDefault();
+  }
+
+  function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
+
+  // modern Chrome requires { passive: false } when adding event
+  var supportsPassive = false;
+  try {
+    window.addEventListener(
+      "test",
+      null,
+      Object.defineProperty({}, "passive", {
+        get: function () {
+          supportsPassive = true;
+        },
+      })
+    );
+  } catch (e) {}
+
+  var wheelOpt = supportsPassive ? { passive: false } : false;
+  var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+
+  // call this to Disable
+  function disableScroll() {
+    window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+    window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+  }
+
+  // call this to Enable
+  function enableScroll() {
+    window.removeEventListener("DOMMouseScroll", preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener("touchmove", preventDefault, wheelOpt);
+    window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+  }
+  useEffect(() => {
+    disableScroll();
+  }, []);
   return (
     <section id="n1">
       <div className="headerContainer">
@@ -25,12 +74,21 @@ const Gkountras = ({ setSelected }) => {
           </p>
         </div>
       </div>
+
       <div>
         <p>
           <br />
         </p>
       </div>
-      <a className="next" href="#n2" onClick={() => setSelected(2)}>
+      <a
+        className="next"
+        href="#n2"
+        onClick={() => {
+          enableScroll();
+          setSelected(2);
+          disableScroll();
+        }}
+      >
         <AiOutlineArrowDown />
       </a>
     </section>
