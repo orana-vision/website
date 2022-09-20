dcca8b077c8e52bf04b80be8e30f21d7f5aae1c3
giannisgkountras <gkountrasioannis@gmail.com>
2022-09-20 05:32:55 +0300

    responsive nav bar fully animated

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -5,9 +5,9 @@ import { BsPerson } from "react-icons/bs";
 import { AiOutlineAppstore } from "react-icons/ai";
 import { HiOutlineMail } from "react-icons/hi";
 import { FiInfo } from "react-icons/fi";
-
+import { FaBars } from "react-icons/fa";
 const Nav = () => {
-  const [scroll, setScroll] = useState(false);
+  const [scroll, setScroll] = useState(true);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       setScroll(window.scrollY < 500);
@@ -19,11 +19,30 @@ const Nav = () => {
   };
 
   function scrollFunction() {
-    if (window.scrollY < 500) {
+    if (window.scrollY < 100) {
       document.getElementById("nav").style.width = "100%";
-    } else {
+      // } else if (window.scrollY < 300) {
+      //   document.getElementById("nav").style.width = "82%";
+      // } else if (window.scrollY < 400) {
+      //   document.getElementById("nav").style.width = "75%";
+      // } else if (window.scrollY < 500) {
+      //   document.getElementById("nav").style.width = "50%";
+    } else if (window.scrollY > 400) {
       document.getElementById("nav").style.width = "25%";
     }
+  }
+  const [x, setX] = useState(0);
+  function myFunction() {
+    if (x === 0) {
+      document.getElementById("dropdown").style.display = "none";
+      setX(1);
+      console.log(x);
+    } else {
+      document.getElementById("dropdown").style.display = "block";
+      setX(0);
+      console.log(x);
+    }
+    console.log(x);
   }
 
   return (
@@ -34,7 +53,37 @@ const Nav = () => {
           <a href="#about">About</a>
           <a href="#experience">FAQ</a>
           <a href="#contact">Contact</a>
-          <a href="#footer">Info</a>{" "}
+          <a href="#footer">Info</a>
+          <div className="selector" id="selector">
+            <button
+              className="selector-button"
+              onClick={() =>
+                document.getElementById("dropdown").style.display === "none"
+                  ? (document.getElementById("dropdown").style.display = "block")
+                  : (document.getElementById("dropdown").style.display = "none")
+              }
+            >
+              <FaBars />
+            </button>
+
+            <ul id="dropdown">
+              <li>
+                <a href="#header">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#experience">FAQ</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li>
+                <a href="#footer">Info</a>
+              </li>
+            </ul>
+          </div>
         </>
       ) : (
         <>
@@ -53,6 +102,36 @@ const Nav = () => {
           <a href="#footer">
             <FiInfo />
           </a>
+          <div className="selector" id="selector">
+            <button
+              className="selector-button"
+              onClick={() =>
+                document.getElementById("dropdown").style.display === "none"
+                  ? (document.getElementById("dropdown").style.display = "block")
+                  : (document.getElementById("dropdown").style.display = "none")
+              }
+            >
+              <FaBars />
+            </button>
+
+            <ul id="dropdown">
+              <li>
+                <a href="#header">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#experience">FAQ</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li>
+                <a href="#footer">Info</a>
+              </li>
+            </ul>
+          </div>
         </>
       )}
     </nav>
