c3881da60f1d93a09449441a50234f1a19ac2ed6
giannisgkountras <gkountrasioannis@gmail.com>
2022-09-20 04:17:02 +0300

    NAVBAR ANIMATION

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 import { RiHomeLine } from "react-icons/ri";
 import { BsPerson } from "react-icons/bs";
@@ -7,25 +7,56 @@ import { HiOutlineMail } from "react-icons/hi";
 import { FiInfo } from "react-icons/fi";
 
 const Nav = () => {
-    return (
-        <nav>
-            <a href="#header">
-                <RiHomeLine />
-            </a>
-            <a href="#about">
-                <BsPerson />
-            </a>
-            <a href="#experience">
-                <AiOutlineAppstore />
-            </a>
-            <a href="#contact">
-                <HiOutlineMail />
-            </a>
-            <a href="#footer">
-                <FiInfo />
-            </a>
-        </nav>
-    );
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY < 500);
+    });
+  }, []);
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (window.scrollY < 500) {
+      document.getElementById("nav").style.width = "100%";
+    } else {
+      document.getElementById("nav").style.width = "25%";
+    }
+  }
+
+  return (
+    <nav id="nav" className={scroll ? "nav-test" : "nav-modern"}>
+      {scroll ? (
+        <>
+          <a href="#header">Home</a>
+          <a href="#about">About</a>
+          <a href="#experience">FAQ</a>
+          <a href="#contact">Contact</a>
+          <a href="#footer">Info</a>{" "}
+        </>
+      ) : (
+        <>
+          <a href="#header">
+            <RiHomeLine />
+          </a>
+          <a href="#about">
+            <BsPerson />
+          </a>
+          <a href="#experience">
+            <AiOutlineAppstore />
+          </a>
+          <a href="#contact">
+            <HiOutlineMail />
+          </a>
+          <a href="#footer">
+            <FiInfo />
+          </a>
+        </>
+      )}
+    </nav>
+  );
 };
 
 export default Nav;
