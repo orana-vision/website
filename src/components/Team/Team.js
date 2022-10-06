73833a7c336ee5f7433c133bf81dd48b177f293b
giannisgkountras <gkountrasioannis@gmail.com>
2022-10-06 23:50:48 +0300

    team member, gkountras seba kemal are obsolete

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "./Team.css";
-import Gkountras from "./Gkountras";
 
-import Seba from "./Seba";
-import Kemal from "./Kemal";
+import MEImg from "../../assets/me.jpg";
+import HORGOSImg from "../../assets/horgos.jpg";
+import KemalImg from "../../assets/kemalidis.jpg";
 
+import translate from "../../utils/translations";
+import TeamMember from "./TeamMember";
 import OranaLogo from "../../assets/orana-logo.png";
 
 import { FiArrowRight, FiArrowLeft } from "react-icons/fi";
 
 import Footer from "../Footer/Footer";
 
-export default function Team() {
+const linkedin_1 = "https://www.linkedin.com/in/ioannis-s-horgos-b5889a21a/";
+const github_1 = "https://github.com/giannishorgos";
+const linkedin_2 = "https://www.linkedin.com/in/ioannis-gkountras-25245723b/";
+const github_2 = "https://github.com/giannisgkountras";
+const linkedin_3 = "https://www.linkedin.com/in/ioannis-gkountras-25245723b/";
+const github_3 = "https://github.com/giannisgkountras";
+
+export default function Team({ language }) {
   return (
     <>
       <div className="top">
@@ -34,7 +43,15 @@ export default function Team() {
               >
                 <FiArrowLeft />
               </button>
-              <Gkountras />
+              {/* <Gkountras /> */}
+              <TeamMember
+                find_me={translate(language).team_member.find_me}
+                name={translate(language).team_member.name_2}
+                imgsrc={MEImg}
+                bio={translate(language).team_member.bio_2}
+                linkedin_link={linkedin_2}
+                github_link={github_2}
+              />
               <button
                 className="next"
                 onClick={() => {
@@ -59,7 +76,15 @@ export default function Team() {
               >
                 <FiArrowLeft />
               </button>
-              <Seba />
+              {/* <Seba /> */}
+              <TeamMember
+                find_me={translate(language).team_member.find_me}
+                name={translate(language).team_member.name_1}
+                imgsrc={HORGOSImg}
+                bio={translate(language).team_member.bio_1}
+                linkedin_link={linkedin_1}
+                github_link={github_1}
+              />
               <button
                 className="next"
                 onClick={() => {
@@ -84,7 +109,15 @@ export default function Team() {
               >
                 <FiArrowLeft />
               </button>
-              <Kemal />
+              {/* <Kemal /> */}
+              <TeamMember
+                find_me={translate(language).team_member.find_me}
+                name={translate(language).team_member.name_3}
+                imgsrc={KemalImg}
+                bio={translate(language).team_member.bio_3}
+                linkedin_link={linkedin_3}
+                github_link={github_3}
+              />
               <button
                 className="next"
                 onClick={() => {
