67c6a9a8c56b8314488e95ab3cf3e84b65f04371
Ioannis Horgos <giannis.21.osfp@gmail.com>
2022-10-07 00:15:05 +0300

    various fixes

--- src/components/Team/TeamMember.js
+++ src/components/Team/TeamMember.js
@@ -1,36 +1,35 @@
 import React from "react";
-import ME from "../../assets/me.jpg";
 
 import "./TeamMember.css";
 import linkedin from "../../assets/linkedin.png";
 import git from "../../assets/github.png";
 
-const Gkountras = ({ find_me, name, imgsrc, bio, linkedin_link, github_link }) => {
-  return (
-    <div className="gkountras-container">
-      <div className="details">
-        <div class="img-hover-zoom img-hover-zoom--quick-zoom">
-          <img src={imgsrc} alt="giannis-gkountras" width="400" height="400" className="profileImage" />
-        </div>
-        <div className="socials find">
-          {find_me}
-          <div className="socials-bg">
-            <a href={linkedin_link} target="_blank" rel="noreferrer">
-              <img src={linkedin} width={"40"} alt="linkedin" />
-            </a>
-            <a href={github_link} target="_blank" rel="noreferrer">
-              <img src={git} width={"40"} alt="github" />
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="details">
-        <p className="title">{name}</p>
+const TeamMember = ({ find_me, name, imgsrc, bio, linkedin_link, github_link }) => {
+    return (
+        <div className="gkountras-container">
+            <div className="details">
+                <div class="img-hover-zoom img-hover-zoom--quick-zoom">
+                    <img src={imgsrc} alt="giannis-gkountras" width="400" height="400" className="profileImage" />
+                </div>
+                <div className="socials find">
+                    {find_me}
+                    <div className="socials-bg">
+                        <a href={linkedin_link} target="_blank" rel="noreferrer">
+                            <img src={linkedin} width={"40"} alt="linkedin" />
+                        </a>
+                        <a href={github_link} target="_blank" rel="noreferrer">
+                            <img src={git} width={"40"} alt="github" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="details">
+                <p className="title">{name}</p>
 
-        <p className="bio">{bio}</p>
-      </div>
-    </div>
-  );
+                <p className="bio">{bio}</p>
+            </div>
+        </div>
+    );
 };
 
-export default Gkountras;
+export default TeamMember;
